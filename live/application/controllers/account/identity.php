d3ddbf60576d29513f5189d60ba4282929306673
Devan <devananda@dyned.com>
2017-10-05 14:00:33 +0700

    Welcome notification for first time login

--- live/application/controllers/account/identity.php
+++ live/application/controllers/account/identity.php
@@ -94,6 +94,32 @@ class identity extends MY_Site_Controller {
 
     // Index
     public function index() {
+
+        $id = $this->auth_manager->userid();
+        
+        $pull_notif = $this->db->select('*')
+                      ->from('user_notifications')
+                      ->where('user_id', $id)
+                      ->get()->result();
+
+        $pull_name = $this->db->select('*')
+                      ->from('user_profiles')
+                      ->where('user_id', $id)
+                      ->get()->result();
+
+        if(!$pull_notif){
+
+            $user_notification = array(
+                'user_id' => $id,
+                'description' => 'Congratulation '.$pull_name[0]->fullname.' and Welcome to DynEd Live.',
+                'status' => 2,
+                'dcrea' => time(),
+                'dupd' => time(),
+            );
+                        
+            $this->user_notification_model->insert($user_notification);
+        }
+
         redirect('account/identity/detail/profile');
     }
 
