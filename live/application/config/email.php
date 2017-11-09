ba0f0f1badd307e94d1312766f8fb72052335388
Devan <devananda@dyned.com>
2017-11-09 12:52:21 +0700

    remove unnecessary comment

--- live/application/config/email.php
+++ live/application/config/email.php
@@ -1,16 +1,3 @@
-<!-- config office365 
-<?php
-/* $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.office365.com';
-$config['smtp_port'] = '587';
-$config['smtp_user'] = 'devananda@dyned.com';
-$config['smtp_pass'] = 'Luya3964';
-$config['charset'] = 'utf-8';
-$config['newline'] = "\r\n";
-$config['crlf'] = "\r\n";
-$config['smtp_crypto'] = 'tls'; */
-?> -->
-
 <?php
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'us2.smtp.mailhostbox.com';
@@ -21,6 +8,4 @@ $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
 $config['newline'] = "\r\n";
 $config['smtp_crypto'] = 'tls';
-
-
 ?>
