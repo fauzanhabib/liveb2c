cbc8219503eccea3907b11539f98aff03b126b09
dyned-rendy <rbustari@dyned.com>
2017-12-27 13:32:01 +0700

    version 1.0.11

--- live/application/views/default/layouts/version.php
+++ live/application/views/default/layouts/version.php
@@ -1,3 +1,3 @@
 <div class="version" style="position: absolute;right: 10px;">
-    <title style="display:block;font-size:12px">version 1.0.10 </title>
+    <title style="display:block;font-size:12px">version 1.0.11 </title>
 </div>
