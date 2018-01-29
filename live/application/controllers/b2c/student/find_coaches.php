e5ac2b8107a20b15120ea838ca23081846ebb728
Devan <devananda@dyned.com>
2018-01-29 11:52:52 +0700

    DELB2C-292, handling partner settings

--- live/application/controllers/b2c/student/find_coaches.php
+++ live/application/controllers/b2c/student/find_coaches.php
@@ -941,11 +941,11 @@ class find_coaches extends MY_Site_Controller {
         $start_time = $convert['start_time'];
         $end_time = $convert['end_time'];
 
-        $check_max_book_coach_per_day = $this->max_book_coach_per_day($coach_id,$date);
-        if(!$check_max_book_coach_per_day){
-            $this->messages->add('This coach has exceeded maximum booked today', 'warning');
-            redirect('student/find_coaches/book_by_multiple_date_index/' . $index);
-        }
+        // $check_max_book_coach_per_day = $this->max_book_coach_per_day($coach_id,$date);
+        // if(!$check_max_book_coach_per_day){
+        //     $this->messages->add('This coach has exceeded maximum booked today', 'warning');
+        //     redirect('student/find_coaches/book_by_multiple_date_index/' . $index);
+        // }
 
         //echo('<pre>');print_r($convert);
         //echo(date('Y-m-d', $convert['date'])); exit;
@@ -2158,11 +2158,11 @@ class find_coaches extends MY_Site_Controller {
         $usertime2_coach = $et_coach+(60*$minutes_coach)-(5*60);
         $end_hour_coach = date("H:i", $usertime2_coach);
 
-        $check_max_book_coach_per_day = $this->max_book_coach_per_day($coach_id,$date);
-        if(!$check_max_book_coach_per_day){
-            $this->messages->add('This coach has exceeded maximum booked today', 'warning');
-            redirect('b2c/student/find_coaches/search/name/');
-        }
+        // $check_max_book_coach_per_day = $this->max_book_coach_per_day($coach_id,$date);
+        // if(!$check_max_book_coach_per_day){
+        //     $this->messages->add('This coach has exceeded maximum booked today', 'warning');
+        //     redirect('b2c/student/find_coaches/search/name/');
+        // }
 
         //print_r(date('Y-m-d', $date)); exit;
 
@@ -2442,6 +2442,7 @@ class find_coaches extends MY_Site_Controller {
         // get setting partner
         // $coach_id = '187';
         // $date = '2016-07-28';
+        $student_id = $this->auth_manager->userid();
         $partner_id = $this->auth_manager->partner_id($coach_id);
 
         // check apakah status setting region allow atau disallow
@@ -2485,7 +2486,8 @@ class find_coaches extends MY_Site_Controller {
 
         // partner setting about student appointment
         // $setting = $this->partner_setting_model->get();
-        $partner_id = $this->auth_manager->partner_id($coach_id);
+        $student_id = $this->auth_manager->userid();
+        $partner_id = $this->auth_manager->partner_id($student_id);
 
         // check apakah status setting region allow atau disallow
         $region_id = $this->auth_manager->region_id($partner_id);
@@ -2507,7 +2509,7 @@ class find_coaches extends MY_Site_Controller {
 
 
       // $appointment_count = count($this->appointment_model->where('coach_id', $coach_id)->where('status not like', 'cancel')->where('status not like', 'temporary')->where('date', date("Y-m-d", $date))->get_all());
-        $student_id = $this->auth_manager->userid();
+        
         $appointment_count = count($this->appointment_model->where('student_id', $student_id)->where('date', date("Y-m-d", $date))->get_all());
 
         // print_r($this->get_date_week($date)); exit;
