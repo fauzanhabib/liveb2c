3df0bb356c946d251dad1f7928235a656de58772
dyned-rendy <rbustari@dyned.com>
2018-01-29 10:58:53 +0700

    adding version 1.0.18

--- live/application/views/default/layouts/version.php
+++ live/application/views/default/layouts/version.php
@@ -1,3 +1,3 @@
 <div class="version" style="position: absolute;right: 10px;">
-    <title style="display:block;font-size:12px">version 1.0.17 </title>
+    <title style="display:block;font-size:12px">version 1.0.18 </title>
 </div>
