eb45cad5f503a8757e30c59fa8dfacecdae683f1
dyned-rendy <rbustari@dyned.com>
2018-02-28 17:50:13 +0700

    update version 1.0.28

--- live/application/views/default/layouts/version.php
+++ live/application/views/default/layouts/version.php
@@ -1,3 +1,3 @@
 <div class="version" style="position: absolute;right: 10px;">
-    <title style="display:block;font-size:12px">version 1.0.27 </title>
+    <title style="display:block;font-size:12px">version 1.0.28 </title>
 </div>
