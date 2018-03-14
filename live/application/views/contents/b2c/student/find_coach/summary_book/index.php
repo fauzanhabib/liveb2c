414d2b064f099168b2e603130014b6046119f0db
dyned-rendy <rbustari@dyned.com>
2018-03-14 11:02:09 +0700

    YT - DELB2C-321

--- live/application/views/contents/b2c/student/find_coach/summary_book/index.php
+++ live/application/views/contents/b2c/student/find_coach/summary_book/index.php
@@ -10,7 +10,7 @@
                 <?php } ?>
                 <div class="dashboard">
                     <div class="dashboard__menubookingcoachresult">
-                        <a href="<?php echo site_url('b2c/student/find_coaches/single_date'); ?>"> 
+                        <a href="<?php echo site_url('b2c/student/find_coaches/single_date'); ?>">
                             <div class="bookkacoach activediv">Book a Coach</div>
                         </a>
                         <a href="">
@@ -46,12 +46,12 @@
                             <div class="bxcoachend">
                                 <label>End Time</label>
                                 <span>
-                                    <?php 
+                                    <?php
                                     $currentDate = strtotime($end_time);
                                     $futureDate = $currentDate-(60*5);
                                     $endtime = date("H:i:s", $futureDate);
 
-                                    echo($endtime); 
+                                    echo($endtime);
                                     ?>
                                 </span>
                             </div>
@@ -59,14 +59,33 @@
                                 <label>Token Cost</label>
                                 <span>
                                     <?php
+                                    $partner_id = $this->auth_manager->partner_id($data_coach[0]->id);
+                                    $region_id = $this->auth_manager->region_id($partner_id);
+
+                                    // check status setting region
+                                    $setting_region = $this->db->select('status_set_setting')->from('specific_settings')->where('user_id',$region_id)->get()->result();
+
+                                    // $setting = $this->db->select('standard_coach_cost,elite_coach_cost, session_duration')->from('global_settings')->where('type','partner')->get()->result();
+
+                                    // jika 0 / disallow
+                                    if($setting_region[0]->status_set_setting == 0){
+                                        $setting = $this->db->select('standard_coach_cost,elite_coach_cost, session_duration')->from('global_settings')->where('type','partner')->get()->result();
+                                        $standard_coach_cost = @$setting[0]->standard_coach_cost;
+                                        $elite_coach_cost = @$setting[0]->elite_coach_cost;
+                                    } else {
+                                        $setting = $this->db->select('standard_coach_cost,elite_coach_cost, session_duration')->from('specific_settings')->where('partner_id',$partner_id)->get()->result();
+                                        $standard_coach_cost = @$setting[0]->standard_coach_cost;
+                                        $elite_coach_cost = @$setting[0]->elite_coach_cost;
+                                    }
+                                    
                                     $token = '';
                                     if($data_coach[0]->coach_type_id == 1){
                                         $token = $standard_coach_cost;
                                     }else if($data_coach[0]->coach_type_id == 2){
-                                        $token = $elite_coach_cost; 
+                                        $token = $elite_coach_cost;
                                     }
 
-                                    echo($token); 
+                                    echo($token);
                                     ?>
                                 </span>
                             </div>
@@ -90,12 +109,12 @@
             </section>
 
             <script>
-                
+
             </script>
 
             <script>
                 $(document).on('touchstart click', '#submit_summary', function () {
-                    
+
                     $('.page__loader').addClass('flex');
 
 
@@ -118,7 +137,5 @@
 
                 $(document).on('touchstart click', '#cancel_summary', function () {
                     location.href = "<?php echo $search_by == 'single_date' ? site_url('b2c/student/find_coaches/book_by_single_date/'.date('Y-m-d', @$date)) : site_url('b2c/student/find_coaches/search/' . $search_by); ?>";
-                });                
+                });
             </script>
-
-
