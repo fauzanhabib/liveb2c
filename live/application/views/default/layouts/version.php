9c99152639ad4cd41a931de52bf5e2f630541d7d
dyned-rendy <rbustari@dyned.com>
2018-04-04 10:47:14 +0700

    update version to 1.0.42

--- live/application/views/default/layouts/version.php
+++ live/application/views/default/layouts/version.php
@@ -1,3 +1,3 @@
 <div class="version" style="position: absolute;right: 10px;">
-    <title style="display:block;font-size:12px">version 1.0.41 </title>
+    <title style="display:block;font-size:12px">version 1.0.42 </title>
 </div>
