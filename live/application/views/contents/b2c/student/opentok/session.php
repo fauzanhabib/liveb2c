e64f18fb240685ee987a8f4ebc65a95863350b7a
irawanwimo <irawandwihastomo@ymail.com>
2018-05-02 16:47:58 +0700

    DELB2C - 350 (Condition for Study Dashboard)

--- live/application/views/contents/b2c/student/opentok/session.php
+++ live/application/views/contents/b2c/student/opentok/session.php
@@ -539,7 +539,7 @@ opacity: 1 !important;
                             </svg>
                             <div class="step__progress__info">
                                 <div class="step__info__label">
-                                  <?php if($gsp->data->total_points_until_today >= $gsp->data->total_points_to_pass){?>
+                                  <?php if($gsp->data->total_points_until_today >= $gsp->data->total_points_expected_today){?>
                                   <!-- kondisi point telah ketemu goal -->
                                   <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                              viewBox="0 0 61.8 61.8" style="enable-background:new 0 0 61.8 61.8;" xml:space="preserve">
@@ -640,13 +640,13 @@ opacity: 1 !important;
                                         </svg>
                           <?php }else{?>
                                         <!-- kondisi point belom ketemu goal -->
-                                        <h5><?php echo ($gsp->data->total_points_to_pass - $gsp->data->total_points_until_today);?></br>points left</h5>
+                                        <h5><?php echo ($gsp->data->total_points_expected_today - $gsp->data->total_points_until_today);?></br>points left</h5>
                           <?php } ?>
                                 </div>
                             </div>
                     </div>
                     </div>
-                  <?php if($gsp->data->total_points_until_today >= $gsp->data->total_points_to_pass){?>
+                  <?php if($gsp->data->total_points_until_today >= $gsp->data->total_points_expected_today){?>
                     <!-- kondisi point telah ketemu goal -->
                     <h5>Congratulation!</h5>
                   <?php }else{?>
