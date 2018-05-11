b9a5f93ef5a60f3e03a72f0e5cd30556607154b3
irawanwimo <irawandwihastomo@ymail.com>
2018-05-11 14:52:35 +0700

    Live-686 and 688

--- live/application/views/contents/b2c/student/opentok/session.php
+++ live/application/views/contents/b2c/student/opentok/session.php
@@ -17,7 +17,7 @@ if(@$user_extract2){
 
 ?>
 <script type="text/javascript" src="<?php echo base_url();?>assets/b2c/lib/jQuery/jquery-2.2.3.min.js"></script>
-<script src='//static.opentok.com/v2/js/opentok.min.js'></script>
+<script src='https://static.opentok.com/v2/js/opentok.min.js'></script>
 <script charset="utf-8">
     var apiKey = '<?php echo $apiKey ?>';
     var sessionId = '<?php echo $sessionId ?>';
@@ -25,7 +25,10 @@ if(@$user_extract2){
     var session = OT.initSession(apiKey, sessionId);
     var publisher;
     var checkcamera;
+
+    initializeSession();
     //Self
+    function initializeSession() {
         session.connect(token, function(error) {
             var publisherproperties = {insertMode: 'append',
                                   width: '100%',
@@ -62,6 +65,7 @@ if(@$user_extract2){
               }
             });
         });
+    }
 
     function toggleOff(){
       $("#videooff").hide();
