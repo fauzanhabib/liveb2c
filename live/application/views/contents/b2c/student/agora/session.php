398a349aba3757e55a24ed4e243ae9912b6aabef
irawanwimo <irawandwihastomo@ymail.com>
2018-06-16 10:07:13 +0700

    Update previous code

--- live/application/views/contents/b2c/student/agora/session.php
+++ live/application/views/contents/b2c/student/agora/session.php
@@ -1236,16 +1236,16 @@ setInterval('checkShare()', 1000);
   // videoSelect.onchange = getDevices();
 </script>
 <script>
-  // var appointment_id = "<?php echo $appointment_id; ?>";
-  // $.post("<?php echo site_url('opentok/live/store_session');?>", { 'appointment_id': appointment_id },function(data) {
-  //   stat_first = data;
-  //   // console.log("==============================");
-  //   // console.log(stat_first);
-  //   if (stat_first == 1) {
-  //     closetab();
-  //   }
-  // });
   var appointment_id = "<?php echo $appointment_id; ?>";
+  $.post("<?php echo site_url('opentok/live/store_session');?>", { 'appointment_id': appointment_id },function(data) {
+    // stat_first = data;
+    // console.log("==============================");
+    // console.log(stat_first);
+    // if (stat_first == 1) {
+    //   closetab();
+    // }
+  });
+
   var stat_check;
 
   var checksess = setInterval(function() {
--- live/application/views/contents/b2c/student/opentok/session.php
+++ live/application/views/contents/b2c/student/opentok/session.php
@@ -968,17 +968,17 @@ opacity: 1 !important;
   }, milisecond);
 </script>
 <script>
-  // var appointment_id = "<?php echo $appointment_id; ?>";
-  // $.post("<?php echo site_url('opentok/live/store_session');?>", { 'appointment_id': appointment_id },function(data) {
-  //   // console.log(data);
-  //   stat_first = data;
-  //   // console.log("==============================");
-  //   console.log(stat_first);
-  //   if (stat_first == 1) {
-  //     closetab();
-  //   }
-  // });
   var appointment_id = "<?php echo $appointment_id; ?>";
+  $.post("<?php echo site_url('opentok/live/store_session');?>", { 'appointment_id': appointment_id },function(data) {
+    // console.log(data);
+    // stat_first = data;
+    // console.log("==============================");
+    // console.log(stat_first);
+    // if (stat_first == 1) {
+    //   closetab();
+    // }
+  });
+
   var stat_check;
 
   var checksess = setInterval(function() {
