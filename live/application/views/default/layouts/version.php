bcc909d9a9625212d0a6e76a5feb6dd8886f11bd
Devan <devananda@dyned.com>
2018-08-02 17:04:57 +0700

    update version 1.0.67

--- live/application/views/default/layouts/version.php
+++ live/application/views/default/layouts/version.php
@@ -1,3 +1,3 @@
 <div class="version" style="position: absolute;right: 10px;">
-    <title style="display:block;font-size:12px">version 1.0.66 </title>
+    <title style="display:block;font-size:12px">version 1.0.67 </title>
 </div>
