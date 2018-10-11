64a5787c078a53974c3338881923f56f53d477fa
Faizal Ditya <dityafaizal99@gmail.com>
2018-10-11 11:27:18 +0700

    DELB2C-554 typo on book coach DELB2C-543 mobile live session fixing

--- live/application/views/contents/b2c/student/find_coach/availability.php
+++ live/application/views/contents/b2c/student/find_coach/availability.php
@@ -152,7 +152,7 @@ if (!@$availability) {
                         <td class="text-center"><?php echo $endtime; ?></td>
                         <td>
                         <?php if(($this->auth_manager->role() != "PRT") && ($this->auth_manager->role() != 'ADM') && ($this->auth_manager->role() != 'RAD')){ ?>
-                            <a href="<?php echo site_url('b2c/student/find_coaches/summary_book/'.$search_by.'/' . $coach_id . '/' . strtotime(@$adate) . '/' . @$availability[$i]['start_time'] . '/' . @$availability[$i]['end_time']); ?>" class="pure-button btn-small btn-white trn" data-trn-key="book2">Book2</a>
+                            <a href="<?php echo site_url('b2c/student/find_coaches/summary_book/'.$search_by.'/' . $coach_id . '/' . strtotime(@$adate) . '/' . @$availability[$i]['start_time'] . '/' . @$availability[$i]['end_time']); ?>" class="pure-button btn-small btn-white trn" data-trn-key="book2">Book</a>
                         <?php } ?>
                         </td>
                     </tr>
